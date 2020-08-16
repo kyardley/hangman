25f1837b74ac28fa3ed103c4e1abad4731140f38
Klynt Yardley <kyardley@Navabi-2.local>
2020-08-16 15:50:00 -0600

    Began developing gameplay

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-#importing the time module
+#import
 import time
 from pics import HANGMANPICS
+from csv import reader
+import random
 
-#welcoming the user
+#Function to display the Hangman Picture
+def hangmanpic(index):
+    print(HANGMANPICS[index])
 
-
+#Function to select word from wordbank
+def selectword():
+    with open('Hangman_wordbank', 'r') as f:
+        csv_reader = reader(f)
+        words = list(csv_reader)
+        selection=random.choice(words[0])
+        return selection
 
 def main():
+    fails=0
+    ALLOWED_FAILS=6
     print ("Welcome to Hangman.")
-    print(HANGMANPICS[0])
+    hangmanpic(fails)
     name = input("What is your name? ")
-
     print ("")
+
+
+
 
     #wait for 1 second
     time.sleep(1)
+    print("Hi, {}. Let's play!".format(name))
+    time.sleep(1)
 
+    word=selectword()
+    length=len(word)
+    print("Your word has {} characters".format(length))
+    print(word)
     print ("Start guessing...")
     time.sleep(0.5)
+
+    while(fails <=ALLOWED_FAILS):
+        print(fails)
+        hangmanpic(fails)
+        fails +=1 
 
 
 if __name__ == "__main__":
