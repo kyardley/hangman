a57416e87aa6bea1e9951b170e58f1b02692deee
Klynt Yardley <kyardley@Navabi-2.local>
2020-08-17 10:04:22 -0600

    Continued developing game

--- main.py
+++ main.py
@@ -14,7 +14,19 @@ def selectword():
         csv_reader = reader(f)
         words = list(csv_reader)
         selection=random.choice(words[0])
+        f.close()
         return selection
+
+def printword(word):
+    temp=''
+    for char in word:
+        temp += char
+        temp += ' '
+    print(temp)
+
+def addchar(word, guess, index):
+    word[index] = guess
+    return word
 
 def main():
     fails=0
@@ -31,18 +43,37 @@ def main():
     time.sleep(1)
     print("Hi, {}. Let's play!".format(name))
     time.sleep(1)
+    SELECTWORD=True
+    while SELECTWORD==True:
+        answer=selectword()
+        length=len(answer)
+        print("Your word has {} characters".format(length))
+        print(answer)
+        if input("Do you want to play with this word? (Y/N)") == "Y":
+            SELECTWORD=False
+    word=list(answer)
+    for i in range(0,length):
+        word[i]='_'
 
-    word=selectword()
-    length=len(word)
-    print("Your word has {} characters".format(length))
-    print(word)
     print ("Start guessing...")
-    time.sleep(0.5)
 
+    time.sleep(0.5)
+    guesses = ''
     while(fails <=ALLOWED_FAILS):
-        print(fails)
         hangmanpic(fails)
-        fails +=1 
+        printword(word)
+        guess=input("Guess a character: ")
+        guesses += guess
+        #check guess
+        index=answer.find(guess)
+        if index != -1:
+            print('Yay')
+            word=addchar(word, guess, index)
+            printword(word)
+        else:
+            fails += 1
+        print(guesses)
+
 
 
 if __name__ == "__main__":
